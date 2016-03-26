3a2a0c3971b0074d16ba2de69ebb1e856905c458
aiyou4love <aiyou4love@outlook.com>
2016-03-26 19:12:14 +0800

    u

--- autopack/Program.cs
+++ autopack/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace autopack
 {
     class Program
     {
-        static void runInitCommand()
+        static T Deserialize<T>(string nFileName)
         {
-            mAbstractCommands[]
-        }
-
-        static void runCommand(string nCommand, string nBundleXml)
-        {
+            T result_;
+            using (FileStream fileStream_ = new FileStream(nFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                XmlSerializer xmlSerializer_ = new XmlSerializer(typeof(T));
+                result_ = (T)xmlSerializer_.Deserialize(fileStream_);
+                fileStream_.Close();
+            }
+            return result_;
         }
 
         static void Main(string[] args)
@@ -24,9 +29,21 @@ namespace autopack
                 Console.ReadKey(true);
                 return;
             }
-            runCommand(args[0], args[1], args[2]);
+            if ("-a" == args[0])
+            {
+                ApkCommand apkCommand_ = Deserialize<ApkCommand>(args[1]);
+                apkCommand_.runCommand(args[2]);
+            }
+            else if ("-u" == args[0])
+            {
+                UpdateCommand updateCommand_ = Deserialize<UpdateCommand>(args[1]);
+                updateCommand_.runCommand(args[2]);
+            }
+            else if ("-f" == args[0])
+            {
+                FinishCommand finishCommand_ = Deserialize<FinishCommand>(args[1]);
+                finishCommand_.runCommand(args[2]);
+            }
         }
-
-        static Dictionary<string, AbstractCommand> mAbstractCommands = new Dictionary<string, AbstractCommand>();
     }
 }
