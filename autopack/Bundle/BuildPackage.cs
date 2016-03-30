127db2172f73b8e54014dbc4b3ded96d9a2cb404
aiyou4love <aiyou4love@outlook.com>
2016-03-30 20:36:39 +0800

    u

--- autopack/Bundle/BuildPackage.cs
+++ autopack/Bundle/BuildPackage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -8,17 +9,68 @@ namespace autopack
     [Serializable]
     public class BuildPackage : SerailizeXml
     {
+        void runDelete(string nDirectory)
+        {
+            if (!Directory.Exists(nDirectory))
+            {
+                return;
+            }
+            DirectoryInfo directoryInfo_ = new DirectoryInfo(nDirectory);
+            foreach (FileInfo fileInfo_ in directoryInfo_.GetFiles())
+            {
+                fileInfo_.Attributes = fileInfo_.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                fileInfo_.Delete();
+            }
+            foreach (DirectoryInfo suDirectoryInfo_ in directoryInfo_.GetDirectories())
+            {
+                runDelete(suDirectoryInfo_.FullName);
+            }
+            directoryInfo_.Attributes = directoryInfo_.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+            directoryInfo_.Delete();
+        }
+
+        void runClear(string nDirectory, Bundle nBundle)
+        {
+            string directory_ = nBundle.mDirectorys[nDirectory];
+            if (!Directory.Exists(directory_))
+            {
+                return;
+            }
+            DirectoryInfo directoryInfo_ = new DirectoryInfo(directory_);
+            foreach (FileInfo fileInfo_ in directoryInfo_.GetFiles())
+            {
+                fileInfo_.Attributes = fileInfo_.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                fileInfo_.Delete();
+            }
+            foreach (DirectoryInfo suDirectoryInfo_ in directoryInfo_.GetDirectories())
+            {
+                runDelete(suDirectoryInfo_.FullName);
+            }
+        }
+
         public void runCommand(Bundle nBundle)
         {
-            mClearDirectory.runClear(nBundle);
+            foreach (string i in mClearFiles)
+            {
+                string directory_ = nBundle.mDirectorys[i];
+                FileInfo fileInfo_ = new FileInfo(directory_);
+                fileInfo_.Attributes = fileInfo_.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                fileInfo_.Delete();
+            }
+
+            foreach (string i in mClearDirectorys)
+            {
+                runClear(i, nBundle);
+            }
 
             foreach (CopyOnce i in mCopyOnces)
             {
                 i.runCopy(nBundle);
             }
         }
+        public List<string> mClearFiles { get; set; }
 
-        ClearDirectory mClearDirectory { get; set; }
+        public List<string> mClearDirectorys { get; set; }
 
         public List<CopyOnce> mCopyOnces { get; set; }
     }
